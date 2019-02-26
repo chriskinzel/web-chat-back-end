1914349c18355b784339812253e0a96868fdf83c
Chris Kinzel <christopher.kinzel@ucalgary.ca>
2019-02-25 23:12:47 -0700

    - Initial commit, everything setup - Not yet started ExpressJS stuff

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+    input: 'src/app.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            banner: '#! /usr/bin/env node'
+        }
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+],
+plugins: [
+    // This custom plugin is needed to fix crappy commonjs module imports
+    {
+        transform ( code, id ) {
+            return code.replace(
+                /(?:import[ \t]+\*[ \t]+as[ \t]+(.*)[ \t]+from[ \t]+['"](.*)['"]|import[ \t]+(.*)[ \t]+=[ \t]+require(['"](.*)['"]))/g,
+                'import * as $1_ from \'$2\';const $1 = $1_;'
+            );
+        }
+    },
+    typescript({
+        typescript: require('typescript'),
+        tsconfigOverride: {
+            compilerOptions: {
+                module: 'es6',
+                sourceMap: false
+            }
+        }
+    }),
+    nodeResolve({
+        jsnext: true,
+        main: true
+    }),
+    commonjs({
+
+    }),
+    terser()
+],
+}
